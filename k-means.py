41a55a8fddd04c707534396214bcb006381bc17a
David Sotto Mayor <ddsmmf@gmail.com>
2022-12-15 10:02:19 -0400

    Adding coordernates to Burger King and McDonald's fastfoods and plotting the chart

--- k-means.py
+++ k-means.py
@@ -2,43 +2,36 @@ import numpy as np  # para manipular os vetores
 from matplotlib import pyplot as plt  # para plotar os gráficos
 from sklearn.cluster import KMeans  # para usar o KMeans
 
-dataset = np.array(
-    # matriz com as coordenadas geográficas de cada loja
-    [[-25, -46],  # são paulo
-     [-22, -43],  # rio de janeiro
-        [-25, -49],  # curitiba
-        [-30, -51],  # porto alegre
-        [-19, -43],  # belo horizonte
-        [-15, -47],  # brasilia
-        [-12, -38],  # salvador
-        [-8, -34],  # recife
-        [-16, -49],  # goiania
-        [-3, -60],  # manaus
-        [-22, -47],  # campinas
-        [-3, -38],  # fortaleza
-        [-21, -47],  # ribeirão preto
-        [-23, -51],  # maringa
-        [-27, -48],  # florianópolis
-        [-21, -43],  # juiz de fora
-        [-1, -48],  # belém
-        [-10, -67],  # rio branco
-        [-8, -63]])  # porto velho
-plt.scatter(dataset[:, 1], dataset[:, 0])  # posicionamento dos eixos x e y
-plt.xlim(-75, -30)  # range do eixo x
-plt.ylim(-50, 10)  # range do eixo y
-plt.grid()  # função que desenha a grade no nosso gráfico
+# matriz com as coordenadas geográficas do McDonald's
+dataset_MC = np.array(
+    [[-3.09778, -60.01229],
+     [-3.10432, -60.02323],
+        [-3.09337, -60.02214],
+        [-3.10105, -60.01133],
+        [-3.07952, -60.07181]])
+
+# matriz com as coordenadas geográficas do Burger King
+dataset_BK = np.array([
+    [-3.11856, -59.98207],
+    [-3.09077, -60.00876],
+    [-3.09194, -60.02219],
+    [-3.10105, -60.01133],
+    [-3.04665, -60.07999],
+    [-3.10094, -60.02384],
+    [-3.02744, -59.97708],
+    [-3.08329, -60.07215],
+    [-2.99563, -60.00292]])
 
 kmeans = KMeans(n_clusters=3,  # numero de clusters
-                # algoritmo que define a posição dos clusters de maneira mais
-                # assertiva
                 init='k-means++', n_init=10,
                 max_iter=300)  # numero máximo de iterações
-pred_y = kmeans.fit_predict(dataset)
+pred_y = kmeans.fit_predict(dataset_BK)
+
 # posicionamento dos eixos x e y
-plt.scatter(dataset[:, 1], dataset[:, 0], c=pred_y)
-plt.xlim(-75, -30)  # range do eixo x
-plt.ylim(-50, 10)  # range do eixo y
-plt.grid()  # função que desenha a grade no nosso gráfico
+plt.scatter(dataset_BK[:, 1], dataset_BK[:, 0], c=pred_y)
+plt.xlim(-60.15, -59.85)
+plt.ylim(-3.25, -2.75)
+plt.grid(True, 'both', 'both')
 plt.scatter(kmeans.cluster_centers_[:, 1], kmeans.cluster_centers_[
-            :, 0], s=70, c='red')  # posição de cada centroide no gráfico
+            :, 0], s=70, c='red')
 plt.show()
